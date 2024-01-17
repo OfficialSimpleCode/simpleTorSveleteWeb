a7127d47b83a7a898ae70f0e3167a8364bb1326b
yehoyada <hvuhsg5@gmail.com>
2024-01-17 20:13:57 +0200

    create components

--- src/routes/orders/+page.svelte
+++ src/routes/orders/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import Navbar from "$lib/components/Navbar.svelte";
-    import { Icon, ChevronRight, Calendar, Clock } from "svelte-hero-icons";
+    import OrderCard from "./components/OrderCard.svelte";
+    import TableRow from "./components/TableRow.svelte";
 
     let orders: Array<Record<string, any>> = [
         {
@@ -57,98 +58,7 @@
                 </thead>
                 <tbody>
                     {#each orders as order}
-                        <tr class="group hover:bg-base-200">
-                            <td>
-                                <div class="flex items-center gap-3">
-                                    <div class="avatar">
-                                        <div
-                                            class="mask mask-squircle w-12 h-12"
-                                        >
-                                            <img
-                                                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                                                alt="business profile"
-                                            />
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="font-bold">
-                                            {order.name}
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="flex flex-col items-start gap-2">
-                                    <div class="font-bold">
-                                        {order.employee.name}
-                                    </div>
-                                    <span class="badge badge-ghost badge-sm">
-                                        {order.employee.role}
-                                    </span>
-                                </div>
-                            </td>
-                            <td class="min-w-[145px]">
-                                <div class="flex flex-col items-start gap-2">
-                                    {order.services}
-                                    <div class="flex items-center gap-1">
-                                        <span
-                                            class="badge badge-ghost badge-sm"
-                                        >
-                                            {order.price} ₪
-                                        </span>
-                                        <span
-                                            class="badge badge-ghost badge-sm"
-                                        >
-                                            {order.duration}
-                                        </span>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <h3 class="text-primary">
-                                    {order.date}
-                                </h3>
-                                <h3 class="text-primary">
-                                    {order.time}
-                                </h3>
-                            </td>
-                            <td>
-                                <label>
-                                    <input
-                                        type="checkbox"
-                                        class="checkbox checkbox-success"
-                                    />
-                                </label>
-                            </td>
-                            <th class="sm:opacity-0 sm:group-hover:opacity-100">
-                                <div
-                                    class="dropdown dropdown-hover dropdown-left"
-                                >
-                                    <div
-                                        tabindex="0"
-                                        role="button"
-                                        class="btn btn-outline m-1"
-                                    >
-                                        Actions
-                                    </div>
-                                    <ul
-                                        class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 fixed z-20"
-                                    >
-                                        <li>
-                                            <button> Load Business </button>
-                                        </li>
-                                        <li>
-                                            <button> Edit </button>
-                                        </li>
-                                        <li>
-                                            <button class="text-error">
-                                                Delete
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </th>
-                        </tr>
+                        <TableRow {order} />
                     {/each}
                 </tbody>
             </table>
@@ -157,85 +67,7 @@
             class="flex flex-col md:hidden max-w-[95%] sm:max-w-[80%] w-full gap-6"
         >
             {#each orders as order}
-                <button class="card bg-base-200 w-full hover:bg-base-300">
-                    <div
-                        class="card-body flex flex-col items-start p-4 max-h-30"
-                    >
-                        <div
-                            class="flex flex-row items-center justify-between w-full"
-                        >
-                            <div class="flex flex-row items-center gap-2">
-                                <div class="avatar">
-                                    <div class="mask mask-squircle w-12 h-12">
-                                        <img
-                                            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                                            alt="business profile"
-                                        />
-                                    </div>
-                                </div>
-                                <div
-                                    class="flex flex-col items-start justify-center text-sm"
-                                >
-                                    <div class="font-bold">{order.name}</div>
-                                    <div class="text-sm opacity-80">
-                                        with {order.employee.name}
-                                    </div>
-                                </div>
-                                <div class="w-5" />
-                                <div
-                                    class="flex flex-col items-start justify-center gap-[6px] text-sm"
-                                >
-                                    <div class="font-bold">
-                                        {order.services}
-                                    </div>
-                                    <div class="flex items-center gap-1">
-                                        <span
-                                            class="badge badge-neutral badge-sm"
-                                            >{order.duration}</span
-                                        >
-                                        <span
-                                            class="badge badge-neutral badge-sm"
-                                            >{order.price} ₪</span
-                                        >
-                                    </div>
-                                </div>
-                            </div>
-
-                            <Icon src={ChevronRight} size="26px" />
-                        </div>
-                        <div
-                            class="flex items-center justify-around flex-row gap-4 w-full"
-                        >
-                            <div class="flex flex-row items-start gap-5">
-                                <div
-                                    class="text-primary flex items-center gap-1"
-                                >
-                                    <Icon src={Calendar} size="26px" />
-                                    {order.date}
-                                </div>
-                                <div
-                                    class="text-primary flex items-center gap-1"
-                                >
-                                    <Icon src={Clock} size="26px" />
-                                    {order.time}
-                                </div>
-                            </div>
-                            <div
-                                class:bg-warning={order.status == "pending"}
-                                class:text-warning-content={order.status ==
-                                    "pending"}
-                                class:bg-success={order.status == "approved"}
-                                class:text-success-content={order.status ==
-                                    "approved"}
-                                class="text-sm rounded-xl p-2 text-center"
-                            >
-                                {order.status == "pending"
-                                    ? "Pending"
-                                    : "Approved"}
-                            </div>
-                        </div>
-                    </div>
-                </button>
+                <OrderCard {order} />
             {/each}
         </div>
     </div>
