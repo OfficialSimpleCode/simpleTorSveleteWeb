f21b669a121adf460a0643138e561010890af46c
yehoyada <hvuhsg5@gmail.com>
2024-01-17 20:44:08 +0200

    update static site

--- docs/_app/immutable/nodes/8.Uv9NV9xR.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.NkIzg_aS.js";import{S as t,i as e}from"../chunks/index.xqhgE7eq.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
