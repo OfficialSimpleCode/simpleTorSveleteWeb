3b187bf74d91100fc10cb2ab3b8a4aa0d036fcb9
Shilo saadon <shilosadon@gmail.com>
2024-01-18 19:22:05 +0200

    vfd

--- src/lib/models/payment_hyp/payment_object.ts
+++ src/lib/models/payment_hyp/payment_object.ts
@@ -1,4 +1,7 @@
+import { defaultCurrency } from "../general/currency_model";
+import { Price } from "../general/price";
+
 abstract class PaymentObject {
-    price: Price = new Price("0", defaultCurrency);
-    createdAt: Date = new Date(0);
-  }
+  price: Price = new Price("0", defaultCurrency);
+  createdAt: Date = new Date(0);
+}
