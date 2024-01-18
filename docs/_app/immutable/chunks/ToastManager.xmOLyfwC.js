9b90cf8978323d3297e6fb21dca2fc5821a63b8c
yehoyada <hvuhsg5@gmail.com>
2024-01-18 18:08:47 +0200

    update static site

---
+++ docs/_app/immutable/chunks/ToastManager.xmOLyfwC.js
@@ -0,0 +1,1 @@
+import{w as o}from"./index._sxbm3PL.js";let t=o(null);function a(e){t.set(e),setTimeout(()=>{t.set(null)},2e3)}export{a as S,t as c};
---
+++ docs/_app/immutable/entry/start.nD0dfROD.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.wi4mCZjo.js";export{t as start};
