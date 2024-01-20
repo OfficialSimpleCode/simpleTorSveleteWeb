9b6d8a18d163abd4649b941b1e36bcc682450e1a
yehoyada <hvuhsg5@gmail.com>
2024-01-20 21:29:52 +0200

    add payment dialog

---
+++ src/routes/business/order/components/PaymentDialog.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import { business } from "$lib/stores/Business.js";
+    export let dialog: HTMLDialogElement;
+    export let amount: number;
+
+    let tip: number = 0;
+    let tipOptions: number[] = [0, 5, 10, 17];
+
+    function onPay() {}
+</script>
+
+<dialog
+    bind:this={dialog}
+    class="modal modal-bottom sm:modal-middle"
+    on:close={() => {
+        // history.back();
+    }}
+>
+    <div class="modal-box bg-base-200 flex flex-col gap-4">
+        <h1 class="text-xl text-center">
+            Pay - {(amount + amount * (tip / 100)).toFixed(2)}{$business
+                .currency.symbol}
+        </h1>
+        <div
+            class="bg-base-300 rounded-lg w-full p-3 flex flex-col items-center gap-4"
+        >
+            <h2 class="text-lg">Tip (calculated on full amount)</h2>
+            <div class="flex items-center justify-around w-full">
+                {#each tipOptions as tipOption}
+                    <button
+                        class="btn btn-outline"
+                        class:btn-secondary={tip == tipOption}
+                        on:click={() => (tip = tipOption)}
+                    >
+                        {tipOption}%
+                    </button>
+                {/each}
+            </div>
+            <!-- <div class="text-lg font-bold">{tip}%</div> -->
+            <input
+                type="range"
+                min="0"
+                max="100"
+                bind:value={tip}
+                class="range"
+            />
+        </div>
+        <div class="modal-action">
+            <button class="btn btn-outline" on:click={() => dialog.close()}>
+                Cancel
+            </button>
+            <button class="btn btn-primary"> Pay </button>
+        </div>
+    </div>
+
+    <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+    </form>
+</dialog>
