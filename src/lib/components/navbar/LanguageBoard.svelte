45ac85d8e0bd902c5d35cd28e8a617e9d905d85c
yehoyada <hvuhsg5@gmail.com>
2024-01-21 19:28:12 +0200

    use div for lang button

--- src/lib/components/navbar/LanguageBoard.svelte
+++ src/lib/components/navbar/LanguageBoard.svelte
@@ -10,15 +10,18 @@
 <ul
     class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box max-w-sm translate-x-24 sm:translate-x-12 min-w-[180px] py-4"
 >
-    {#each languages as language}
+    {#each languages as language, i}
         <li>
-            <button
+            <div
+                role="button"
+                tabindex="0"
                 class="flex items-center justify-between"
                 on:click={() => chooseLang(language)}
+                on:keypress={() => chooseLang(language)}
             >
                 {$_(language.name)}
                 <img class="w-9 rounded" src={language.flag} alt="flag" />
-            </button>
+            </div>
         </li>
     {/each}
 </ul>
