63cabaf2f74d885860a79d88b3d34f1552fd9fcc
yehoyada <hvuhsg5@gmail.com>
2024-01-22 20:04:36 +0200

    controle document direction

--- src/lib/components/navbar/LanguageBoard.svelte
+++ src/lib/components/navbar/LanguageBoard.svelte
@@ -2,10 +2,18 @@
     import { locale, _ } from "svelte-i18n";
     export let languages: Array<Record<string, string>>;
 
+    let rtlLocals: string[] = ["he", "ar"];
+    let direction: "ltr" | "rtl" = "rtl";
+    document.dir = direction;
+
     function chooseLang(language: Record<string, string>) {
         locale.set(language.locale);
+        direction = rtlLocals.includes(language.locale) ? "rtl" : "ltr";
+        document.dir = direction;
     }
 </script>
+
+<svelte:document />
 
 <ul
     class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box max-w-sm translate-x-24 sm:translate-x-12 min-w-[180px] py-4"
