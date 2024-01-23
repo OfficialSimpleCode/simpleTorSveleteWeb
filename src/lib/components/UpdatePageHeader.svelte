5de2b87d972b7d0e38541962c57ccb54448ad58f
yehoyada <hvuhsg5@gmail.com>
2024-01-23 16:25:47 +0200

    polish update password page

---
+++ src/lib/components/UpdatePageHeader.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import BackButton from "./BackButton.svelte";
+    import InfoTooltipButton from "./InfoTooltipButton.svelte";
+
+    export let title: string;
+    export let helpMessage: string;
+</script>
+
+<div class="flex items-center justify-between w-full pt-4" style="direction: ltr;">
+    <InfoTooltipButton message="{helpMessage}" />
+    <div>
+        <h1 class="text-xl text-center">{title}</h1>
+    </div>
+    <BackButton />
+</div>
