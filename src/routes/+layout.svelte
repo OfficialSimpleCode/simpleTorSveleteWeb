a2d2fd3f4e93387d4bd5ec9bcb4740ddfe5f0984
Amit Mor Yosef <49193268+AmitMorYosef@users.noreply.github.com>
2024-01-24 22:16:40 +0200

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,9 +31,6 @@
         const firebaseApp =
             getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
          LoadAppHelper.GI().loadApp();
-
-        
-
         
     }
 
