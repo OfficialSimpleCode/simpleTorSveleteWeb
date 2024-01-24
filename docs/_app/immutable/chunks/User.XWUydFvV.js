2a36a98bbaddf6efa66563aab0ae9111db23e240
yehoyada <hvuhsg5@gmail.com>
2024-01-24 16:26:41 +0200

    update static site

--- docs/_app/immutable/chunks/User.XWUydFvV.js
+++
@@ -1,1 +0,0 @@
-import{w as r}from"./index.6-72uIJN.js";import"./index.esm2017.wWOXa-jD.js";import"./business_initializer.l6UkpdXT.js";let i=r();export{i as u};
--- docs/_app/immutable/chunks/Workers.pbhAjj7F.js
+++
@@ -1,1 +0,0 @@
-import{w as e}from"./index.6-72uIJN.js";import"./business_initializer.l6UkpdXT.js";import"./index.esm2017.wWOXa-jD.js";let t=e(null);function l(o){t.set(o),setTimeout(()=>{t.set(null)},2e3)}let i=e();export{l as S,t as c,i as w};
