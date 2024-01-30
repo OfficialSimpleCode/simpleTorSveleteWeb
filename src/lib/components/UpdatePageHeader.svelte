01dbd135d6908bf7e3ceb6ace5078b719ac42ddf
yehoyada <hvuhsg5@gmail.com>
2024-01-30 17:46:28 +0200

    reopen profile modal after field update

--- src/lib/components/UpdatePageHeader.svelte
+++ src/lib/components/UpdatePageHeader.svelte
@@ -4,12 +4,16 @@
 
     export let title: string;
     export let helpMessage: string;
+    export let onBack: CallableFunction = () => {};
 </script>
 
-<div class="flex items-center justify-between w-full pt-4" style="direction: ltr;">
-    <InfoTooltipButton message="{helpMessage}" />
+<div
+    class="flex items-center justify-between w-full pt-4"
+    style="direction: ltr;"
+>
+    <InfoTooltipButton message={helpMessage} />
     <div>
         <h1 class="text-xl text-center">{title}</h1>
     </div>
-    <BackButton />
+    <BackButton {onBack} />
 </div>
