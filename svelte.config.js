2dc3dd72ab46c324d408c4a12f576e6db573f60c
Shilo saadon <shilosadon@gmail.com>
2024-02-13 20:33:31 +0200

    vfe

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const dev = "production" === "development";
+// const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,13 +12,13 @@ const config = {
   kit: {
     adapter: adapter({
       strict: false,
-      pages: "docs",
-      assets: "docs",
+      // pages: "docs",
+      // assets: "docs",
     }),
-    paths: {
-      // change below to your repo name
-      base: dev ? "" : "/your-repo-name",
-    },
+    // paths: {
+    //   // change below to your repo name
+    //   base: dev ? "" : "/your-repo-name",
+    // },
     // // hydrate the <div id="svelte"> element in src/app.html
     // target: "#svelte"
   },
