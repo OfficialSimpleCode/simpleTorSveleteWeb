f721d2328e1cf5e5385bec4161dd75aa568b154c
yehoyada <hvuhsg5@gmail.com>
2024-02-13 18:09:26 +0200

    default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,17 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: ["default", "light", "dark"],
+    default: {
+      "primary": "#ef00ff",
+      "secondary": "#005dff",
+      "accent": "#ff0000",
+      "neutral": "#150f00",
+      "base-100": "#102d22",
+      "info": "#00c3ff",
+      "success": "#b8dd0a",
+      "warning": "#ebca00",
+      "error": "#ff9ca3",
+    },
   },
 };
