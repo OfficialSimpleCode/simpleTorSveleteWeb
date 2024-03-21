f7148015ee851ec26d153abb118471f39e113066
Amit Mor Yosef <49193268+AmitMorYosef@users.noreply.github.com>
2024-03-21 11:20:32 +0200

    .

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import Navbar from "$lib/components/navbar/Navbar.svelte";
 </script>
 
 <div class="h-full w-full">
+  <Navbar />
   <div class="pt-10">
     <slot />
   </div>
